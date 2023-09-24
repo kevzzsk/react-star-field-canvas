4ba130fc10bc402b96de9ad8c3b1de3e1cbdc376
Cezar Pauxis <pauxiscezar@gmail.com>
2023-09-24 02:28:45 -0300

    fix: fix package files

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    copyPublicDir: false,
     lib: {
       entry: "./src/index.ts",
-      name: "ReactStarFieldCanvas",
+      formats: ["es"],
     },
     rollupOptions: {
       external: ["react", "react-dom"],
@@ -18,5 +20,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: [react(), dts({ include: ["src"] })],
 });
